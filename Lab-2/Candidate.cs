3af336e5032d2698555ef18f7837c0d35b4f649b
Viken27 <149919990+Viken27@users.noreply.github.com>
2024-12-12 09:51:55 +0530

    Add files via upload

---
+++ Lab-2/Candidate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Staff
+{
+    public string Name;
+    public string Department;
+    public string Designation;
+    public int Experience;
+    public double Salary;
+
+    public void GetStaffDetails()
+    {
+
+            Console.Write("Enter Name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter Department: ");
+            Department = Console.ReadLine();
+            Console.Write("Enter Designation: ");
+            Designation = Console.ReadLine();
+            Console.Write("Enter Experience: ");
+            Experience = int.Parse(Console.ReadLine());
+            Console.Write("Enter Salary: ");
+            Salary = double.Parse(Console.ReadLine());
+
+            if (Designation.ToLower() == "hod")
+            {
+                Console.WriteLine($"Name: {Name}\nSalary: {Salary}");
+            }
+        }
+    }
+
