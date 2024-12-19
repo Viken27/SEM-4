2e57dcfaef8746820a6df9aa069780e8b04c20b0
Viken27 <149919990+Viken27@users.noreply.github.com>
2024-12-19 10:58:02 +0530

    Update BankAcount.cs

--- Lab-2/BankAcount.cs
+++ Lab-2/BankAcount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,35 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lab_2
 {
-    internal class Candidate
+    internal class BankAccount
     {
-        public int ID;
-        public string Name;
-        public int Age;
-        public float Weight;
-        public float Height;
+        public string AccountNo;
+        public string Email;
+        public string UserName;
+        public string AccountType;
+        public double AccountBalance;
 
-        public void GetCandidateDetails()
+        public void GetAccountDetails()
         {
-            Console.Write("Enter ID: ");
-            ID = int.Parse(Console.ReadLine());
-            Console.Write("Enter Name: ");
-            Name = Console.ReadLine();
-            Console.Write("Enter Age: ");
-            Age = int.Parse(Console.ReadLine());
-            Console.Write("Enter Weight: ");
-            Weight = float.Parse(Console.ReadLine());
-            Console.Write("Enter Height: ");
-            Height = float.Parse(Console.ReadLine());
+            Console.Write("Enter Account Number: ");
+            AccountNo = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            Email = Console.ReadLine();
+            Console.Write("Enter Username: ");
+            UserName = Console.ReadLine();
+            Console.Write("Enter Account Type: ");
+            AccountType = Console.ReadLine();
+            Console.Write("Enter Account Balance: ");
+            AccountBalance = double.Parse(Console.ReadLine());
         }
 
-        public void DisplayCandidateDetails()
+        public void DisplayAccountDetails()
         {
-            Console.WriteLine($"Candidate ID: {ID}");
-            Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Age: {Age}");
-            Console.WriteLine($"Weight: {Weight}");
-            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"\nAccount Details:\nAccount No: {AccountNo}\nEmail: {Email}\nUsername: {UserName}\nAccount Type: {AccountType}\nAccount Balance: {AccountBalance}");
         }
     }
 }
